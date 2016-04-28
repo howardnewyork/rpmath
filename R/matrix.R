b585eb594dba2e8516265b139f94187dd5000afb
howardnewyork <howard@zail.org>
2016-04-28 09:52:10 -0400

    Updated minor changes

--- R/matrix.R
+++ R/matrix.R
@@ -1,23 +1,13 @@
-#### Deep Matrix
+#### R + Math
 #### Version 0.1.0
 
-
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
 
 #' @import dplyr
 NULL
 
 
-#' @useDynLib deepmatrix
+#' @useDynLib rpmath
 #' @importFrom Rcpp sourceCpp evalCpp
 NULL
 
